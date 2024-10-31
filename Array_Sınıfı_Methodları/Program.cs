18784b2b484d1227cd262f9389a61fca3329d8d4
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-10-31 22:00:55 +0300

    Array_Sınıfı_Methodları dersi eklendi

---
+++ Array_Sınıfı_Methodları/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int[] numbers = {10,20,30,40,50,60,70,80,90,100};
+
+Array.Sort(numbers);
+Array.Reverse(numbers);
+
+foreach (var item in numbers)
+{
+    Console.WriteLine(item);
+}
+
+Array.Clear(numbers,8,2);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+int index = Array.IndexOf(numbers,40);
+
+Array.Resize<int>(ref numbers,12);
+
+Console.WriteLine(index);
