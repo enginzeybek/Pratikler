ea45ebb8ad2f4f6491548d05c1f2b8889a1bb58e
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-10-31 21:16:36 +0300

    Döngülerle_Dizi_Kullanımı dersi eklendi

---
+++ Döngülerle_Dizi_Kullanımı/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] renkler = new string[5];
+
+renkler[0] = "RED";
+renkler[1] = "YELLOW";
+renkler[2] = "GREEN";
+renkler[3] = "BLUE";
+renkler[4] = "PİNK";
+
+string[] hayvanlar = {"DONKEY","CAT","COW","DOG","MOUSE","BAT"};
+
+Console.WriteLine(hayvanlar[2]);
+
+for (int i = 0; i < renkler.Length; i++)
+{
+    Console.WriteLine(renkler[i]);
+}
+
+foreach (var item in hayvanlar)
+{
+    Console.WriteLine(item);
+}
