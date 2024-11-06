cf304ee3df94daf4717dc4cb770aa44779a83f87
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-11-06 23:28:15 +0300

    Metot_Overloading dersi eklendi

---
+++ Metot_Overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Out parametreler
+
+string sayi = "999";
+int outSayi;
+
+bool sonuc = int.TryParse(sayi,out outSayi);
+
+if (sonuc)
+{
+    Console.WriteLine("BAŞARILI");
+}
+else {
+    Console.WriteLine("BAŞARISIZ");
+}
+
+Metotlar metot = new Metotlar();
+
+metot.topla(8,9,out int c);
+
+int data = 89;
+
+metot.ekranaYazdir(data);
+
+public class Metotlar
+{
+    public void topla(int a,int b,out int c)
+    {
+        c = a + b;
+        Console.WriteLine(c);
+    }
+
+    public void ekranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void ekranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+}
