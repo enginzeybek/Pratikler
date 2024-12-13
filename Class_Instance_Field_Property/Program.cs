882a4ae5d088f51556da07a32ddedc04984ecbb9
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-12-13 14:52:36 +0300

    Class_Instance_Field_Property dersi eklendi

---
+++ Class_Instance_Field_Property/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Order 
+{
+    public int topla(int sayi1,int sayi2) 
+    {
+        int result = sayi1 + sayi2; // VARİABLE
+
+        return result;
+    }
+
+    public int OrderID { get; set; } // PROPERTY
+
+    private string kelime; // FİELD
+
+    /*PUBLİC=Herkese açık.
+      PRİVATE=Sadece tanımlandığı class içinde geçerli.
+      İNTERNAL=Aynı proje (assembly) içinde geçerli.
+      PROTECTED=Sadece tanımlandığı class ve alt classlardan (inheritance) erişilebilir. */
+}
