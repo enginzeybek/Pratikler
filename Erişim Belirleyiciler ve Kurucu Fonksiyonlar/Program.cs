72f2944b95a4c3eb3adaa8e75b31cb4622d2c303
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-12-13 16:23:07 +0300

    Erişim Belirleyiciler ve Kurucu Fonksiyonlar dersi eklendi

---
+++ Erişim Belirleyiciler ve Kurucu Fonksiyonlar/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Worker worker = new Worker("ENGİN ZEYBEK",32);
+Worker worker2 = new Worker('E');
+
+Console.WriteLine(worker.Ad + " " + worker.Yas + " " + worker2.Cinsiyet);
+
+public class Worker
+{
+    public string? Ad;
+
+    public int Yas;
+
+    public char Cinsiyet;
+    public Worker(string ad,int yas)
+    {
+        Ad = ad;
+        Yas = yas;
+    }
+
+    public Worker(char cinsiyet)
+    {
+        Cinsiyet = cinsiyet;
+    }
+}
