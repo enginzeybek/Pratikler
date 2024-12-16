313bf772bdc0a3e2d8fce4803e4ee122c2a0fc20
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-12-16 21:23:22 +0300

    Enum dersi eklendi

---
+++ Enum/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Car car = new Car();
+
+car.Name = "BMW";
+
+car.color = Colors.MAVİ;
+
+Console.WriteLine("Araba= " + car.color + " " + car.Name);
+
+object greet = "Merhaba";
+
+string stGreet = greet.ToString();
+
+Console.WriteLine(greet.GetType());
+
+Console.WriteLine((int)Colors.MAVİ);
+
+public enum Colors
+{
+    KIRMIZI,
+    SARI,
+    MAVİ,
+    YEŞİL,
+    PEMBE,
+    SİYAH,
+    BEYAZ
+}
+
+public class Car
+{
+    public string? Name;
+    public Colors color;
+}
