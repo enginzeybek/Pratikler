d24e7d1c6d039455b5fa07d2d051cb3b4df127a3
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2024-12-16 16:08:07 +0300

    Encapsulation ve Property Kavramı dersi eklendi

---
+++ Encapsulation ve Property Kavramı/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Ogrenci ogr = new Ogrenci();
+
+ogr.name = "irem aytepe";
+
+//ogr.county = "AYDIN"; SADECE OKUNABİLİR, DEĞER ATANAMAZ.
+
+Console.WriteLine(ogr.name);
+
+
+public class Ogrenci
+{
+    private string? Name;
+
+    private string? County = "TRABZON";
+    public string name
+    {
+        get { return Name; }
+        set { Name = value.ToUpper(); }
+    }
+
+    public string county
+    {
+        get => County;
+    }
+    
+}
