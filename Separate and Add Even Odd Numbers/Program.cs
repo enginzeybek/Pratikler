a7601e8510d510be3ee43dda4a94a61eb45ed4c0
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2025-07-05 13:03:11 +0300

    Tek ve çift sayıların toplamını ekrana yazdıran uygulama

---
+++ Separate and Add Even Odd Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Virgül ile ayırarak tek ve çift sayılar giriniz");
+
+string? numbers = Console.ReadLine();
+
+string[] stringNumbers = numbers.Split(",");
+
+int[] ArrayNumbers = Array.ConvertAll(stringNumbers, int.Parse);
+
+int EvenNumbers = 0;
+
+int OddNumbers = 0;
+
+for (int i = 0; i < ArrayNumbers.Length; i++)
+{
+
+    if (ArrayNumbers[i] % 2 == 0)
+    {
+        EvenNumbers += ArrayNumbers[i];
+    }
+    else if (ArrayNumbers[i] % 2 == 1)
+    {
+        OddNumbers += ArrayNumbers[i];
+    }
+}
+
+Console.WriteLine("TEK SAYILARIN TOPLAMI: " + OddNumbers
++ " " + "ÇİFT SAYILARIN TOPLAMI " + EvenNumbers);
+
