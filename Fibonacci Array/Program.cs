ba8c69c5d8efe8c8ff3f0fcf939e60c246e15d61
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2025-08-02 16:14:55 +0300

    Fibonacci Array uygulaması eklenmiştir

---
+++ Fibonacci Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir Sayı Giriniz");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first = 0;
+int second = 1;
+
+Console.Write(first + " " + second + " ");
+
+for (int i = 2; i < number; i++)
+{
+    int next = first + second;
+    Console.Write(next + " ");
+    first = second;
+    second = next;
+}
+
+
