eca35531b5e303def201379381379676c69decd5
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2025-08-15 21:07:47 +0300

    Kullanıcının girdiği sayının rakamları toplamını bulan uygulama

---
+++ Total of Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.WriteLine("Bir pozitif sayı giriniz");
+
+    string? strNumber = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(strNumber))
+    {
+        Console.WriteLine("LÜTFEN POZİTİF BİR SAYI GİRİNİZ.");
+        continue;
+    }
+    else if (!strNumber.All(char.IsDigit))
+    {
+        Console.WriteLine("LÜTFEN SADECE POZİTİF SAYI GİRİNİZ.");
+        continue;
+    }
+    char[] charArrayNumbers = strNumber.ToArray();
+
+    int[] intArrayNumbers = Array.ConvertAll<char, int>(charArrayNumbers, c => int.Parse(c.ToString()));
+
+    int sum = 0;
+
+    for (int i = 0; i < intArrayNumbers.Length; i++)
+    {
+        sum += intArrayNumbers[i];
+    }
+
+    Console.WriteLine("GİRİLEN RAKAMIN RAKAMLARI TOPLAMI= " + sum);
+
+}
+
