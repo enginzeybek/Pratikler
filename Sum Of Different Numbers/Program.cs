b83a5226537a41a6b0b4f345301df9a31c9d556b
Engin Zeybek <engin_zeybek.tb@hotmail.com>
2025-08-22 21:29:13 +0300

    Farklı sayıların toplamını alma uygulaması

---
+++ Sum Of Different Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Ardaşık sayılar giriniz. Sayıları virgülle ayırınız");
+
+    string? strNumbers = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(strNumbers))
+    {
+        Console.WriteLine("Boş giriş yapılamaz!");
+    }
+
+    string[] strArrayNumbers = strNumbers.Split(",");
+
+    List<int> listNumbers = new List<int>();
+
+    foreach (var item in strArrayNumbers)
+    {
+        if (int.TryParse(item, out int numbers))
+        {
+            listNumbers.Add(numbers);
+        }
+        else
+        {
+            Console.WriteLine("Geçerli sayı giriniz");
+        }
+    }
+    
+    int sumOfUnique = listNumbers.Distinct().Sum();
+
+    Console.WriteLine($"Farklı sayıların toplamı: {sumOfUnique}");
+}
+
+
+
+
+
+
+
